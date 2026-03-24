947640a8d089eb497cdd0dfa799ed73a121715e4
Silvia <silgadi@gmail.com>
2026-03-24 19:01:47 +0100

    1f3qf

---
+++ Assets/Scripts/SilviaScripts/Scene/SC_ChangeIntensity.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SC_ChangeIntensity : MonoBehaviour
+{
+    // Cambia progresivamente el valor del color a blanco
+    // Una vez que es blanco espera 3 segundos
+    // Te manda a la escena del juego
+    
+}
---
+++ Assets/Scripts/SilviaScripts/Scene/SC_InitialFlickeerAnimation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SC_InitialFlickeerAnimation : MonoBehaviour
+{
+    // Para el movimiento del jugador y su interaccion
+    // Realiza un flicker con una animacion en el ca
+    // Permite el movimiento del jugador
+    
+}
